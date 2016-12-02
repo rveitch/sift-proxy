7f66acd52507c96ec7af8961447e4f9e70d6c74f
ryan.veitch <ryan.r.veitch@gmail.com>
2016-12-02 12:40:52 -0600

    Code Cleanup

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 var express = require('express');
 var request = require('request');
-//require('request-debug')(request);
-//var bodyParser = require('body-parser');
 var app = express();
 
 var port = Number(process.env.PORT || 3000);
 var apiServerHost = (process.env.ELASTIC_URL || 'http://127.0.0.1:9200/fccnn/_search');
 
-//app.use(bodyParser.urlencoded({extended: false}));
-//app.use(bodyParser.json());
-
-
-// no-cors example: http://blog.javascripting.com/2015/01/17/dont-hassle-with-cors/
 app.use('/', function(req, res, body) {
 	// short-circuit favicon requests for easier debugging
 	if (req.url != '/favicon.ico') {
@@ -29,8 +22,6 @@ app.use('/', function(req, res, body) {
 
 		// send your request
 	  var url = apiServerHost + req.url;
-		//console.log('req.url: ' + req.url);
-		//req.pipe(request(url)).pipe(res);
 		req.pipe(request({
 		    uri  : url,
 		    auth : {
@@ -49,22 +40,9 @@ app.use('/', function(req, res, body) {
 	}
 });
 
-
-/*
-if (!body) : filter the body field from the uri request
-if (body) : filter body field from the soure fields
-*/
-
-///////////////////////////////////////////////////////////////////////////////
-
 // Server Listen
 app.listen(port, function () {
 	console.log('App server is running on http://localhost:' + port);
 	console.log('Heroku config variable - ELASTIC_URL: ' + process.env.ELASTIC_URL);
 	console.log('apiServerHost: ' + apiServerHost);
 });
-
-// Server frontpage
-/*app.get('/', function (req, res) {
-    res.send('Hello World');
-});*/
